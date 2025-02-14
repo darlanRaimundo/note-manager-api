1b1c5c7baa400c90b443d0851baa6140c351566c
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-02-14 13:36:40 -0300

    Config cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
-      allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
+      methods: 'GET,HEAD,OPTIONS,POST,PUT',
+      allowedHeaders:
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     },
   });
   await app.listen(process.env.PORT ?? 3000);
