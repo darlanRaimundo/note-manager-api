067ec38d03c12fd968c84ad6dc611eff4a00274a
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-02-13 22:37:38 -0300

    remove test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log(`Servidor rodando na porta ${process.env.PORT ?? 3000}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
