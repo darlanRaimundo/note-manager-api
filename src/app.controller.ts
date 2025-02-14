eafe8b5d7546f1535da99c2289296eeb4ab5a71d
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-02-13 21:43:03 -0300

    Fix default route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/constants';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getRunning(): string {
     return this.appService.getRunning();
