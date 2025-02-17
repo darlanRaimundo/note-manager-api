6a5c5142c22908f789a068b6b4a302df17d5f871
Darlan Raimundo <darlanraimundo123@gmail.com>
2025-02-17 20:36:17 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'https://note-manager-front.vercel.app/',
+      origin: '*',
+      methods: '*',
+      allowedHeaders: '*',
       credentials: true,
-      methods: 'GET,HEAD,OPTIONS,POST,PUT',
-      allowedHeaders:
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     },
   });
   await app.listen(process.env.PORT ?? 3000);
